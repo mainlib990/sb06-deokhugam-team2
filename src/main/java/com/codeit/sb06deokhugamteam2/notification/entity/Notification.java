72519786852d1e4ad05c3475c8b6167a7699f00b
3Park <lovehyunju7@naver.com>
2025-11-26 13:48:21 +0900

    feat Notification 엔티티 생성
    - DB연결 후, Entity 자동생성을 통해 생성 및 수정 (타임 관련 컬럼들 설정, id 설정 등)
    - 이슈 : #46

---
+++ src/main/java/com/codeit/sb06deokhugamteam2/notification/entity/Notification.java
@@ -0,0 +1,57 @@
+package com.codeit.sb06deokhugamteam2.notification.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotNull;
+import java.time.Instant;
+import java.util.UUID;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+@Getter
+@Setter
+@Entity
+@EntityListeners(AuditingEntityListener.class)
+@Table(name = "notifications")
+public class Notification {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  @Column(name = "id", nullable = false)
+  private UUID id;
+
+  @NotNull
+  @Column(name = "user_id", nullable = false)
+  private UUID userId;
+
+  @NotNull
+  @Column(name = "review_id", nullable = false)
+  private UUID reviewId;
+
+  @NotNull
+  @Column(name = "review_title", nullable = false, length = Integer.MAX_VALUE)
+  private String reviewTitle;
+
+  @NotNull
+  @Column(name = "content", nullable = false, length = Integer.MAX_VALUE)
+  private String content;
+
+  @NotNull
+  @LastModifiedDate
+  @Column(name = "confirmed_at", nullable = false)
+  private Instant confirmedAt;
+
+  @NotNull
+  @CreatedDate
+  @Column(name = "created_at", nullable = false)
+  private Instant createdAt;
+
+}
